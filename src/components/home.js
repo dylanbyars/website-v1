b1d18f942b4f7f3c7aa81a6bf318d76f66087b5f
Dylan <dylan@dylanbyars.com>
2017-01-31 16:59:26 -0500

    added inner div for content (coming soon)

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return (
             <div className="container__home">
-                HOME
+                <div>HOME</div>
             </div>
         )
     }

0ec8121bf677b020ba15d23a1d017873e8bebd4f
Dylan <dylan@dylanbyars.com>
2017-01-31 16:56:44 -0500

    updated routing for initial testing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import Main from './views/main'
-import Home from './components/home';
+import Root from './components/root';
 
-render(
-  <Router history={browserHistory}>
-    <Route component={Main}>
-      <Route path="/" component={Home} />
-    </Route>
-  </Router>,
-  document.getElementById('root')
-);
+import Home from './components/home';
+import { About } from './components/about';
+import { Experience } from './components/experience';
+// import Projects from './components/projects';
+import { Contact } from './components/contact'
+
+// import PeriodicTable from './projects/periodic-table-app';
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={browserHistory}>
+        <Route path={"/"} component={Root}>
+          <IndexRoute component={Home} />
+          <Route path={"about"} component={About} />
+          <Route path={"experience"} component={Experience} />
+          <Route path={"contact"} component={Contact} />
+        </Route>
+      </Router>
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'));

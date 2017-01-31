7844ac3eff95744fbc4f3f63ea040c373ee89eef
Dylan <dylan@dylanbyars.com>
2017-01-31 18:41:16 -0500

    moved export to the SkillChart.js file and removed this

--- src/components/skill-chart/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import SkillChart from './components/skill-chart';
-import './index.css';
-
-render(
-  <SkillChart />,
-  document.querySelector('.root')
-);

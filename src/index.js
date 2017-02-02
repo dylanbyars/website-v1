039441a7c7ef3ddf000531d731edf7a62f6d3858
Dylan <dylan@dylanbyars.com>
2017-02-01 23:37:14 -0500

    Updated element paths and router routes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-
-import Root from './components/root';
-
-import Home from './components/home';
-import { About } from './components/about';
-import { Experience } from './components/experience';
-import SkillChart from './components/skill-chart/SkillChart';
-import { Contact } from './components/contact'
+import Root from './components/root/Root';
+import Home from './components/home/Home';
+import { Experience } from './components/experience/Experience';
+import Projects from './components/projects/Projects';
+import Contact from './components/contact/Contact'
 
 // import PeriodicTable from './projects/periodic-table-app';
 
-class App extends Component {
+class SweetWebsite extends Component {
   render() {
     return (
       <Router history={browserHistory}>
         <Route path={"/"} component={Root}>
           <IndexRoute component={Home} />
-          <Route path={"about"} component={About} />
           <Route path={"experience"} component={Experience} />
-          <Route path={"skill-chart"} component={SkillChart} />
+          <Route path={"projects"} component={Projects} />
           <Route path={"contact"} component={Contact} />
         </Route>
       </Router>
@@ -28,4 +24,4 @@ class App extends Component {
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<SweetWebsite />, document.getElementById('root'));

5107cd4cb29fc1d817c077e47106e38b8cccd8e3
Dylan <dylan@dylanbyars.com>
2017-02-02 08:17:25 -0500

    Removed skill chart. Moved it to the Experience page.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import SkillChart from './skill-chart/SkillChart';
 
 class Home extends Component {
     render() {
         return (
             <div className="container__home">
-                <div className="home__blurb">
-                </div>
-                <div className="home__skill-chart">
-                    <SkillChart />
-                </div>
+                <div className="home__blurb">Blurb</div>
             </div>
         )
     }

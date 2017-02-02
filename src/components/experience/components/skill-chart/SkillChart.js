366cd53614573024130ee21de4da92cb70431174
Dylan <dylan@dylanbyars.com>
2017-02-02 08:17:09 -0500

    moved into new sub folder in experience folder

---
+++ src/components/experience/components/skill-chart/SkillChart.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import ReactTransitionGroup from 'react-addons-transition-group';
+import SkillBar from './components/SkillBar';
+import { Key } from './components/Key';
+import './skill-chart.scss';
+
+class SkillChart extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      skills: [{
+        "HTML5": "virtuoso"
+      }, {
+        "CSS3": "virtuoso"
+      }, {
+        "Javascript": "cozy"
+      }, {
+        "React": "cozy"
+      }, {
+        "Redux": "learning"
+      }, {
+        "Polymer": "learning"
+      }, {
+        "jQuery": "learning"
+      }, {
+        "Wordpress": "fiddled"
+      }, {
+        "Webpack": "learning"
+      }, {
+        "Bower": "fiddled"
+      }, {
+        "Git": "cozy"
+      }, {
+        "Node.js": "learning"
+      }, {
+        "Python": "fiddled"
+      }, {
+        "Firebase": "read"
+      }, {
+        "C++": "read"
+      }]
+    }
+
+    this.setSkillBars = this.setSkillBars.bind(this)
+
+  }
+
+  setSkillBars() {
+    let delay = 0
+    const skillBars = this.state.skills.map(skill => {
+      let tech = Object.keys(skill)[0]
+      let prow = skill[tech]
+      delay += .15
+      return (
+        <ReactTransitionGroup>
+          <SkillBar key={tech} technology={tech} prowess={prow} delay={delay} />
+        </ReactTransitionGroup>
+      )
+    })
+    return skillBars
+  }
+
+  render() {
+    return (
+      <div className="container__skill-chart">
+        <div className="skill-chart__header">Skills</div>
+        <div className="skill-chart__widget">{this.setSkillBars()}</div>
+        <Key />
+      </div>
+    )
+  }
+}
+
+export default SkillChart;

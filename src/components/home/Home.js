4659b93675598d9ed90fb1f3400b4813f77fa669
Dylan <dylan@dylanbyars.com>
2017-02-03 00:18:32 -0500

    imported sass file

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './home.scss';
 
 class Home extends Component {
     render() {
         return (
             <div className="container__home">
-                <div className="home__blurb">Blurb</div>
+                <div className="home__blurb">Home Blurb</div>
             </div>
         )
     }

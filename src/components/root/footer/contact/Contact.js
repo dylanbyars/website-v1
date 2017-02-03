1d369bbccdfe633be6b1cfcdf222553f0b24a355
Dylan <dylan@dylanbyars.com>
2017-02-03 00:20:46 -0500

    added to footer element

---
+++ src/components/root/footer/contact/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './contact.scss';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className="container__contact">
+                <form className="contact-form">
+                    
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Contact;

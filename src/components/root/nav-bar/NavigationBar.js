6c7f12f9523541de0d7147903ad9c02201de46a7
Dylan <dylan@dylanbyars.com>
2017-02-04 17:45:44 -0500

    putting aside for now..

---
+++ src/components/root/nav-bar/NavigationBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import './nav-bar.scss';
+
+class NavBar extends Component {
+
+  render() {
+
+    return (
+      <nav className="container__navbar">
+        <div className="navbar__items">
+          <div className="navbar__item navbar__home">
+            <Link to="/" className="navbar__link">Dylan Byars</Link>
+          </div>
+          <div className="navbar__pages">
+            <div className="navbar__item">
+              <Link to="/experience" className="navbar__link" activeClassName="navbar__link--active">Experience</Link>
+            </div>
+            <div className="navbar__item">
+              <Link to="/projects" className="navbar__link" activeClassName="navbar__link--active">Projects</Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;

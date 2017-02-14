467025100c84e00746386c3766626d4294f8ed39
Dylan <dylan@dylanbyars.com>
2017-02-13 22:51:49 -0500

    initial commit

---
+++ src/components/about/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export const About = () => {
+    return (
+        <div className="container__about">
+            Check out blogs I read and other cool blarfff
+        </div>
+    )
+}

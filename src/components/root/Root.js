080234f7422a9de3ed4491e15b25bddb3d64543b
Dylan <dylan@dylanbyars.com>
2017-02-15 20:52:58 -0500

    swapped out the header for just the ToggleableNav. May undo that though as the navigation isn't very clear on non-mobile screens...

--- src/components/root/Root.js
+++ src/components/root/Root.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header } from './components/header/Header';
+import ToggleableNav from './components/ToggleableNav';
 import { Footer } from './components/footer/Footer';
 
 export const Root = ({ children }) => {
@@ -7,7 +7,7 @@ export const Root = ({ children }) => {
     return (
         <div className="container__root">
             
-            <Header />
+            <ToggleableNav />
             
             <div className="root__content">
                 {children}

702d422b00f0250067748e5cac573f6dc7c389b5
Dylan <dylan@dylanbyars.com>
2017-02-20 22:56:23 -0500

    updated routes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import { Root } from './components/root/Root';
+import { HomeLayout } from './components/layouts/HomeLayout';
+import { RoamingLayout } from './components/layouts/RoamingLayout';
 import Home from './components/home/Home';
 import { Experience } from './components/experience/Experience';
 import Projects from './components/projects/Projects';
-import { About } from './components/about/About';
+import PeriodicTableApp from './components/projects/components/periodic-table-app/PeriodicTableApp';
+import WeatherApp from './components/projects/components/weather-app/WeatherApp';import { About } from './components/about/About';
 import './styles/main.scss';
 
 class SweetWebsite extends Component {
   render() {
     return (
+
       <Router history={browserHistory}>
-        <Route path={"/"} component={Root}>
+        <Route path="/" component={HomeLayout}>
           <IndexRoute component={Home} />
-          <Route path={"experience"} component={Experience} />
-          <Route path={"projects"} component={Projects} />
-          <Route path={"about"} component={About} />
+          <Route component={RoamingLayout}>
+            <Route path="experience" component={Experience} />
+            <Route path="projects" component={Projects}>
+              <Route path="periodic-table-app" component={PeriodicTableApp} />
+              <Route path="weather-app" component={WeatherApp} />
+            </Route>
+            <Route path="about" component={About} />
+          </Route>
         </Route>
       </Router>
+
     )
   }
 }

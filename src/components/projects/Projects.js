89ffeae94d96d8b4d733a707218ecd9d32228db6
Dylan <dylan@dylanbyars.com>
2017-02-20 22:58:15 -0500

    started upgrade

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,33 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import { ProjectsHeader } from './components/ProjectsHeader';
-import { ProjectPreview } from './components/ProjectPreview';
-import PeriodicTable from './components/periodic-table-app/PeriodicTable';
-import WeatherApp from './components/weather-app/WeatherApp';
+// import { ProjectPreview } from './components/ProjectPreview';
+// import PeriodicTable from './components/periodic-table-app/PeriodicTable';
+// import WeatherApp from './components/weather-app/WeatherApp';
 
 class Projects extends Component {
 
     render() {
         return (
-            <div className="container__projects">
+            <div className="projects">
 
-                <ProjectsHeader />
+                <ProjectsHeader />             
                 
-                <div className="projects__intro">
-                    <div className="projects__title">Take a look at some of these projects</div>
-                    <div className="projects__blurb">Yarghhhh</div>
-                </div>                
-                
-                <div className="projects__content">
-                    <div className="containter__project">
-                        <ProjectPreview>
-                            <PeriodicTable />
-                        </ProjectPreview>
-                    </div>
-                    <div className="container__project">
-                        <ProjectPreview>
-                            <WeatherApp />
-                        </ProjectPreview>
-                    </div>
+                <Link to="/projects/periodic-table-app">Periodic Table App</Link>
+
+                <Link to="/projects/weather-app">Weather App</Link>
+
+                <div className="project">
+                    {this.props.children}
                 </div>
 
             </div>

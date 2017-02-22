7642ea1987e8c36f8a54f237a4b11552fd0a5356
Dylan <dylan@dylanbyars.com>
2017-02-21 23:23:09 -0500

    reorganization

--- src/components/layouts/components/footer/Footer.js
+++ src/components/layouts/components/footer/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
 // import { Quoter } from './components/quoter/Quoter';
 // icons
-import codepenLogo from './icons/codepen-logo--border.svg';
-import githubLogo from './icons/github-logo.svg';
-import linkedinLogo from './icons/linkedin-logo.svg';
-import emailIcon from './icons/email-icon.svg';
+import codepenLogo from '../icons/codepen-logo--border.svg';
+import githubLogo from '../icons/github-logo.svg';
+import linkedinLogo from '../icons/linkedin-logo.svg';
+import emailIcon from '../icons/email-icon.svg';
 
 export const Footer = () => {
 
@@ -30,6 +30,10 @@ export const Footer = () => {
                     <a href="mailto:dylan@dylanbyars.com">
 <img className="footer__social-img" src={emailIcon} alt="Holler" /></a>
                 </div>
+
+                {/*<div className="footer__thanks">
+                    <p>Proudly powered by <a href="http://">rice and beans</a>.</p>
+                </div>*/}
 
             </div>
 

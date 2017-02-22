6a8a21dbd184fab88771d09e2d780f0eb2dee6b1
Dylan <dylan@dylanbyars.com>
2017-02-21 23:20:03 -0500

    added content

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,8 +3,15 @@ import React from 'react';
 export const About = () => {
     return (
         <div className="about">
-            Check out blogs I read and other cool blarfff
-            Include this somehow => https://www.youtube.com/watch?v=xJMD5R8stRc
+
+            <div className="page__header">
+                <div className="page__title">About</div>
+            </div>
+
+            <p className="about__plea">Thanks for checking out my site! I'm an aspiring junior frontend web developer itching to join a team of internet-talkers in the Northern Virginia/Washington D.C. area.</p>
+
+            <p>Check out blogs I read and other cool blarfff
+            Include this somehow => https://www.youtube.com/watch?v=xJMD5R8stRc</p>
         </div>
     )
 }

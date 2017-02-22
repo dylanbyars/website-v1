2bb79746714402a598ef8f0caf5248ea9bc6cfb9
Dylan <dylan@dylanbyars.com>
2017-02-21 23:23:30 -0500

    started working on the Projects landing page

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { ProjectsHeader } from './components/ProjectsHeader';
 // import { ProjectPreview } from './components/ProjectPreview';
 // import PeriodicTable from './components/periodic-table-app/PeriodicTable';
 // import WeatherApp from './components/weather-app/WeatherApp';
 
 class Projects extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            pageTitle: 'Projects',
+            projectOpen: false
+        }
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(newTitle) {
+        this.setState({pageTitle: newTitle})
+    }
 
     render() {
+
         return (
-            <div className="projects">
+            <div className="projects page">
 
-                <ProjectsHeader />             
+                <div className="page__header">
+                    <div className="page__title">{this.state.pageTitle}</div>
+                </div>
                 
-                <Link to="/projects/periodic-table-app">Periodic Table App</Link>
+                <Link to="/projects/periodic-table-app" onClick={() => this.handleClick('Periodic Table App')}>Periodic Table App</Link>
 
-                <Link to="/projects/weather-app">Weather App</Link>
+                <Link to="/projects/weather-app" onClick={() => this.handleClick('Weather App')}>Weather App</Link>
 
                 <div className="project">
                     {this.props.children}

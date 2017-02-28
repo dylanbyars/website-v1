0cd6e332b3122b8540eb1ade3f5c53b7ae4ef99e
Dylan <dylan@dylanbyars.com>
2017-02-27 23:43:33 -0500

    simplified markup

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,45 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import CellularAutomaton from './components/CellularAutomaton';
 
-class Home extends Component {
+export const Home = () => {
 
-    render() {
+    return (
+        <div className="home">
 
-        return (
-            <div className="home">
+            <div className="home__navigation">
 
-                <div className="home__background">
-                    <CellularAutomaton />
-                </div>
+                <Link className="home__nav-link" activeClassName="home__nav-link--active" to="experience">Experience</Link>
 
-                <div className="home__foreground">
+                <Link className="home__nav-link" activeClassName="home__nav-link--active" to="/projects">Projects</Link>
 
-                    <div className="home__navigation">
-
-                        <div className="home__nav-item">
-                            <Link className="home__nav-link" activeClassName="home__nav-link--active" to="experience">Experience</Link>
-                        </div>
-
-                        <div className="home__nav-item">
-                            <Link className="home__nav-link" activeClassName="home__nav-link--active" to="/projects">Projects</Link>
-                        </div>
-
-                        <div className="home__nav-item">
-                            <Link className="home__nav-link" activeClassName="home__nav-link--active" to="/about">About</Link>
-                        </div>
-
-                    </div>
-
-                    <div className="home__db">Dylan Byars</div>
-
-                    <div className="home__hustle">frontend web developer</div>
-
-                </div>
+                <Link className="home__nav-link" activeClassName="home__nav-link--active" to="/about">About</Link>
 
             </div>
-        )
-    }
-}
 
-export default Home;
+            <div className="home__db">Dylan Byars</div>
+
+            <div className="home__hustle">frontend web developer</div>
+
+        </div>
+    )
+}

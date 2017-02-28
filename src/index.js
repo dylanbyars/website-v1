93a716006d489b38001eebff91f35bb7426231fb
Dylan <dylan@dylanbyars.com>
2017-02-27 23:40:25 -0500

    added About page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { HomeLayout } from './components/layouts/HomeLayout';
 import { RoamingLayout } from './components/layouts/RoamingLayout';
-import Home from './components/home/Home';
+import { Home } from './components/home/Home';
 import { Experience } from './components/experience/Experience';
-import Projects from './components/projects/Projects';
+import { Projects } from './components/projects/Projects';
 import PeriodicTableApp from './components/projects/components/periodic-table-app/PeriodicTableApp';
-import WeatherApp from './components/projects/components/weather-app/WeatherApp';import { About } from './components/about/About';
+import WeatherApp from './components/projects/components/weather-app/WeatherApp';
+import { About } from './components/about/About';
 import './styles/main.scss';
 
 class SweetWebsite extends Component {

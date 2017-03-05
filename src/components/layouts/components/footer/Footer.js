cbba61834a4172fb26c9f56440655763a0768cf0
Dylan <dylan@dylanbyars.com>
2017-03-05 16:52:00 -0500

    removed mail icon and updated markup

--- src/components/layouts/components/footer/Footer.js
+++ src/components/layouts/components/footer/Footer.js
@@ -4,31 +4,27 @@ import React from 'react';
 import codepenLogo from '../icons/codepen-logo--border.svg';
 import githubLogo from '../icons/github-logo.svg';
 import linkedinLogo from '../icons/linkedin-logo.svg';
-import emailIcon from '../icons/email-icon.svg';
 
 export const Footer = () => {
 
     return (
 
         <footer className="footer">
+
+            <p className="footer__email">dylan@dylanbyars.com</p>
             
             <div className="footer__social-media">
 
                 <div className="footer__social-item">
-                    <a className="footer__social-link" href="https://www.github.com/dbyars"><img className="footer__social-img" src={githubLogo} alt="Github Logo" /></a>
+                    <a href="https://www.github.com/dbyars"><img className="footer__social-img" src={githubLogo} alt="Github Logo" /></a>
                 </div>
 
                 <div className="footer__social-item">
-                    <a className="footer__social-link"href="https://www.codepen.io/dylanbyars"><img className="footer__social-img" src={codepenLogo} alt="Codepen Logo" /></a>
+                    <a href="https://www.codepen.io/dylanbyars"><img className="footer__social-img" src={codepenLogo} alt="Codepen Logo" /></a>
                 </div>
 
                 <div className="footer__social-item">
-                    <a className="footer__social-link" href="https://www.linkedin.com/in/dylanbyars"><img className="footer__social-img" src={linkedinLogo} alt="LinkedIn Logo" /></a>
-                </div>
-
-                <div className="footer__social-item">
-                    <a href="mailto:dylan@dylanbyars.com">
-<img className="footer__social-img mail-icon" src={emailIcon} alt="Holler" /></a>
+                    <a href="https://www.linkedin.com/in/dylanbyars"><img className="footer__social-img" src={linkedinLogo} alt="LinkedIn Logo" /></a>
                 </div>
 
             </div>

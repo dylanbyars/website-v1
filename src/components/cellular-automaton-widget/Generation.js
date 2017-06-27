4d90b3bd61d64be532dba30f359678132245d40f
Dylan <dylan@dylanbyars.com>
2017-06-26 22:58:18 -0400

    moved

---
+++ src/components/cellular-automaton-widget/Generation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Generation = ({ cellStates }) => {
+
+  const generation = cellStates.map(cellState => {
+    return <span className={cellState}></span>
+    }
+    )
+
+  return <div className="generation">{generation}</div>
+}
